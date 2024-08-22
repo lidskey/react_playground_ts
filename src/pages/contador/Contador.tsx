e5faaa5ff3a18b76323893048ee56855ab6e0e57
Liara Cristina <lidskey@hotmail.com>
2024-08-22 20:34:55 -0300

    react

---
+++ src/pages/contador/Contador.tsx
@@ -0,0 +1,29 @@
+﻿import { useState } from "react"
+
+function Contador() {
+
+  /* const[nome_estado,funcao_setState] = useState(valor_inicial_do_estado)
+ */
+  
+  /* estado valor= react */
+  const [valor, setValor] = useState(0);
+
+  /* typescript */
+  function handleClick() {
+    setValor(valor + 1);
+  }
+
+/* hook */
+  return (
+    <div>
+      <h2>Componente Contador</h2>
+
+      <p>O valor atual do meu estado é: {valor}</p>
+
+      <button onClick={handleClick}>Adicionar +1</button>
+
+    </div>
+  )
+}
+
+export default Contador
---
+++ src/pages/tarefa/Tarefa.tsx
@@ -0,0 +1,27 @@
+﻿import { useEffect, useState } from "react"
+
+function Tarefa() {
+
+  const [completed, setCompleted] = useState(false);
+  const [tarefa, setTarefa] = useState('');
+  /* dentro dos colchetes são os estados */
+  
+  useEffect(() => {
+    if (completed) {
+      setTarefa('Parabéns! Você concluiu a Tarefa!')
+    } else {
+      setTarefa('')
+    }
+  },[completed])
+
+  return (
+    <div>
+      <h2>Componente Tarefa</h2>
+      <h3>{tarefa}</h3>
+      <p>Conclua a Tarefa</p>
+      <button onClick={() => setCompleted(!completed)}>Concluir Tarefa</button>
+    </div>
+  )
+}
+
+export default Tarefa
